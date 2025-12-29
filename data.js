12a9618b6de966c96cad6d01b8cdde13e1cf79df
85dlauto85 <david.avagimov.e@gmail.com>
2025-12-29 01:46:42 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+// ==================== ДАННЫЕ АВТОМОБИЛЕЙ ====================
+// Добавляй новые машины ТОЛЬКО в этот файл!
+// Формат: id, марка, модель, год, цена, пробег, город, двигатель, коробка, кузов, цвет, описание, фото (массив), телефон, WhatsApp
+
+const cars = [
+  {
+    id: "001",
+    make: "Hyundai",
+    model: "Getz",
+    year: 2006,
+    price: 3990,
+    mileage: 218000,
+    engine: "1.5 diesel",
+    transmission: "mécanique",
+    body: "citadine",
+    color: "noir",
+    description: "Excellent état, bien entretenu, carnet d'entretien complet. Économique, idéal pour la ville.",
+    photos: [
+      "images/Hyundai-Getz-2006/1.jpg",
+      "images/Hyundai-Getz-2006/2.jpg",
+      "images/Hyundai-Getz-2006/3.jpg",
+      "images/Hyundai-Getz-2006/4.jpg",
+      "images/Hyundai-Getz-2006/5.jpg",
+	  "images/Hyundai-Getz-2006/6.jpg",
+	  "images/Hyundai-Getz-2006/7.jpg"
+    ],
+    contactPhone: "+33 6 35 95 79 41",
+    contactWhatsApp: "33635957941"
+  }
+];
